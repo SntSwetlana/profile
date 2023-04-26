7e88020840e93ea869f19a636b40f5e5fcfbd247
SntSwetlana <snt.swetlana@gmail.com>
2023-04-26 11:38:50 +0300

    fix Hire me

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from "next/link";
 import { CircularText } from './Icons'
 
 const HireMe = () => {
@@ -7,6 +8,13 @@ const HireMe = () => {
     items-center justify-center overflow-hidden'>
         <div className='w-48 h-auto flex items-center justify-center relative '>
             <CircularText className={'fill-dark animate-spin-slow'}/>
+            <Link href="mailto:snt.swetlana@gmail.com" 
+            className='flex items-center justify-center 
+            absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
+            bg-dark text-light shadow-md border border-solid border-dark
+            w-28 h-28  rounded-full text-2xl '>
+              Hire Me
+            </Link>
         </div>
     </div>
   )
