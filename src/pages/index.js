7524d4e91d1ea1a945a2472c9fedd210c87be5e4
SntSwetlana <snt.swetlana@gmail.com>
2023-04-29 14:54:41 +0300

    fix button

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,15 +42,13 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link href='/dummy.pdf' target={'_blank'}
-                className="flex tems-center bg-dark text-light p-2.5 px-6 
+                className="flex items-center bg-dark text-light p-2.5 px-6 
                 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark 
                 border-2 border-solid border-transparent hover:border-dark
                 dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
-                hover:dark:border-light md:p-2 md:px-4 md:text-base
-                "
-
+                hover:dark:border-light md:p-2 md:px-2 md:text-base"
                 download={true}
-                >Resume <LinkArrow className={"w-6 ml-1"}/>
+                > Resume <LinkArrow className={"w-6 ml-1"}/>
                 </Link>
                 <Link href="mailto: snt.swetlana@gmail.com" target={"_blank"}
                 className="ml-4 text-lg font-medium
