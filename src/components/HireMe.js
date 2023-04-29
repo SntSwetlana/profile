5d02141b9b89c39e6867f5692263861e9eb5117e
SntSwetlana <snt.swetlana@gmail.com>
2023-04-29 21:39:09 +0300

    minor bugses fixed

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -6,13 +6,14 @@ const HireMe = () => {
   return (
     <div className="fixed left-4 bottom-4 flex 
                     items-center justify-center overflow-hidden 
+                    z-30
                     md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute 
                     sm:right-0 sm:top-0
+                  
                     ">
         <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
             <CircularText className={"fill-dark animate-spin-slow dark:fill-light"}/>
-            <Link href="mailto:snt.swetlana@gmail.com" 
-              className="flex items-center justify-center 
+            <span className="flex items-center justify-center 
               absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
               w-28 h-28  rounded-full text-2xl font-semibold
               cursor-pointer
@@ -21,8 +22,11 @@ const HireMe = () => {
               dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light 
               md:w-12 md:h-12 md:text-[10px]
               " >
+            <Link href="mailto:snt.swetlana@gmail.com" 
+              >
                 Hire Me
             </Link>
+            </span>
         </div>
     </div>
   )
--- src/pages/products.js
+++ src/pages/products.js
@@ -70,7 +70,7 @@ const Project = ({type, title, img, link, github}) => {
         <article className="w-full flex flex-col items-center justify-center rounded-2xl 
         border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light xs:p-4">
            <div className="absolute top-0 -right-4 -z-10 w-[101%] h-[102%] rounded-[2rem] bg-dark
-           rounded-br-3xl dark:bg-light md:right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]" />
+           rounded-br-3xl dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]" />
             <Link href={link} target = "_blank"
             className='w-full cursor-pointer overflow-hidden rounded-lg '
             >
