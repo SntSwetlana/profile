d5ae95a36a7a2a86b5824dba38e3d591600b9ce3
SntSwetlana <snt.swetlana@gmail.com>
2023-04-29 15:01:27 +0300

    test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -173,15 +173,18 @@ const NavBar = () => {
             <DribbleIcon />
           </motion.a>
           <button
-            onClick={() => setMode(mode === "light" ? "dark" : "light")}
-            className={`ml-3 flex items-center justify-center rounded-full p-1
-          ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
-          `}
-          >
-            {
-              mode === "light" ? (<SunIcon className={"fill-dark "} />) : (<MoonIcon className={" fill-dark"} />)
-            }
-          </button>
+              onClick={() => setMode(mode === "light" ? "dark" : "light")}
+              className={`ml-3 flex items-center justify-center rounded-full p-1
+      ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
+      `}
+            >
+              {mode === "light" ? (
+            <SunIcon className={"fill-dark"} />
+          ) : (
+            <MoonIcon className={"fill-dark"} />
+          )}
+            </button>
+
         </nav>
       </motion.div>
  ) : null
